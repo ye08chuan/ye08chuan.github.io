5f154bddc4cc5e0050243e1a8c3a5bdd84ad1da9
ye08chuan <xihaoye65@gmail.com>
2025-05-06 12:19:06 +0200

    Add files via upload

---
+++ original.js
@@ -0,0 +1,12 @@
+console.log("___________________");
+console.log("Hola máquina");
+console.log("Preparado para tu nuevo codigo");
+console.log("___________________");
+
+
+
+
+
+
+
+
---
+++ trabajo.js
@@ -0,0 +1,13 @@
+console.log("___________________");
+console.log("Hola máquina");
+console.log("Preparado para tu nuevo codigo");
+console.log("___________________");
+
+
+
+  
+
+
+
+
+
